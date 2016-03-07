cb10c706bb1a85dd1650bd529469f899fcb30f9e
btswt22 <sherrywang@uchicago.edu>
2016-03-06 20:46:37 -0600

    added simulated data and indoor outdoor sensor dropdown

--- backend/load_sample_data.sh
+++ backend/load_sample_data.sh
@@ -5,3 +5,9 @@ done
 for line in $(head 1_20160304.txt); do
   echo "$line"
 done
+
+
+curl -i -XPOST 'http://24.14.64.152/write?db=hourly_counter_db' --data-binary '	hourly_counter,sensor=1 counter=1 1457291416000000000';
+
+
+SELECT (SELECT sum(counter) FROM hourly_counter_db..hourly_counter WHERE time >= '" + query_dt + "' and time <= '" + query_dt + "' + 1d GROUP BY time(30m)
